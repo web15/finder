f38a3654e0990efac2fac0097ddf2c8418bbc6a2
Charles Marks <clmarks15@gmail.com>
2018-12-19 15:01:51 -0500

    completed

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ searchUser.addEventListener('keyup', (e) => {
           } else {
               // Show Profile
                 ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
           }
         })
     } else {
--- github.js
+++ github.js
@@ -4,15 +4,21 @@ class Github {
     constructor()   {
         this.client_id = '0e13d9a486e2ebc321a8';
         this.client_secret = 'f763b3435f9cb93eb2aca5b21ee578ba249654c4';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
 
+        const repos = await repoResponse.json();
+
         return {
-            profile
+            profile, repos
         }
     }
 }
